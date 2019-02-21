411764ae61b5fc54867197df1f0e481ca9b7fd6d
josephSEmbedded <joseph.shenouda@rutgers.edu>
2019-02-21 18:07:44 -0500

    Created project

---
+++ Lab_1/Lab_1.srcs/sources_1/new/clock_div.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 02/21/2019 05:51:00 PM
+-- Design Name: 
+-- Module Name: clock_div - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity clock_div is
+--  Port ( );
+end clock_div;
+
+architecture Behavioral of clock_div is
+
+begin
+
+
+end Behavioral;
